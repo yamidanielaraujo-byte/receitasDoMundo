b9d8d4d32a1fb9a28b54125aadf7b79e65695d8d
Yami <yami.daniel.araujo@gmail.com>
2026-01-14 16:51:45 +0000

    Tipo de letra diferente

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,18 @@
 // Header.js
 import Link from 'next/link';
+import { Dancing_Script } from 'next/font/google';
+
+const dancingScript = Dancing_Script({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 const Header = () => {
   return (
-    <header className="bg-[#1a1a1a] text-white py-4 px-6">
+    <header className="bg-gray-800 text-white py-4 px-6">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         {/* Logo/Title on the left */}
-        <Link href="/" className="text-xl font-bold hover:text-gray-300">
+        <Link href="/" className="text-3xl font-bold hover:text-gray-300" style={{ fontFamily: 'Dancing Script, cursive' }}>
           Receitas do Mundo
         </Link>
 
