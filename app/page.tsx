37c61acfce8e538838bd7db67b9e1c17398ecf6a
Yami <yami.daniel.araujo@gmail.com>
2026-01-14 13:17:06 +0000

    Página Inicial
    Página inicial com globo e slogan interativo

---
+++ app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Globe from "@/components/ui/globe";
+import { TextSpotlight } from "@/components/ui/text-spotlight";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center gap-10 py-20">
+
+      <Globe
+        rotateCities={["new york", "london", "tokyo", "dubai"]}
+        rotationSpeed={3000}
+        markers={[
+          { location: [40.7128, -74.006], size: 0.2 },
+          { location: [51.5074, -0.1278], size: 0.2 },
+          { location: [35.6762, 139.6503], size: 0.2 },
+          { location: [25.2048, 55.2708], size: 0.2 },
+        ]}
+        glowColor={[0.1, 0.8, 1]}
+        markerColor={[0.1, 0.8, 1]}
+        className="max-w-200"
+      />
+      
+      <Globe />
+      
+      <TextSpotlight
+        textClassName="text-xl md:text-4xl font-semibold"
+        text="Sabores que viajam – descubra o mundo em cada prato!"
+        spotlightColor="255, 255, 255"
+        spotlightArea={90}
+        spotlightSize={100}
+      />
+    </div>
+  );
+}
